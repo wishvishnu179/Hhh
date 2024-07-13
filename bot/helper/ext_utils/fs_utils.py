509718ec09e67d6748dec152f79f55f8f141bc2c
Daemon9990 <meruguvasu963@gmail.com>
2024-07-13 15:18:49 +0530

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -181,7 +181,7 @@ async def join_files(path):
 
 
 async def edit_metadata(listener, base_dir: str, media_file: str, outfile: str, metadata: str = ''):
-    cmd = ['ffmpeg', '-hide_banner', '-ignore_unknown', '-i', media_file, '-metadata',  f'title={metadata}',  '-metadata', f'CREDITS={metadata}','-metadata:s:v',
+    cmd = ['ffmpeg', '-hide_banner', '-ignore_unknown', '-i', media_file, '-metadata',  f'title={metadata}',  '-metadata', f'CREDITS={metadata}', '-metadata', f'Comment={metadata}', '-metadata:s:v',
            f'title={metadata}', '-metadata:s:a', f'title={metadata}', '-metadata:s:s', f'title={metadata}', '-map', '0:v:0?',
            '-map', '0:a:?', '-map', '0:s:?', '-c:v', 'copy', '-c:a', 'copy', '-c:s', 'copy', outfile, '-y']
     listener.suproc = await create_subprocess_exec(*cmd, stderr=PIPE)
